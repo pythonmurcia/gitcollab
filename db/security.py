452b7a1f207295151e871de8b9daddfa75d6a780
PabloCorbCon <pablocorbcon@gmail.com>
2020-09-30 15:43:23 +0200

    El archivo de seguridad security.py ha sido creado y explicado dentro de /db/

---
+++ db/security.py
@@ -0,0 +1,4 @@
+# En este archivo se almacenan todas las funciones respectivas a la seguridad de la base de datos, como el cifrado de datos.
+# Tareas relaccionadas con este archivo en nuestro repositorio de GitHub:
+#   #12 Función para encriptar los datos del usuario en la base - 5
+#   #13 Función para desencriptar los datos de las bases de datos - 5
