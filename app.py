64541ffbc2a6b8e0c4a63fab43e85c2f3de306c6
Pablo Corbalán <pablocorbcon@gmail.com>
2020-09-30 16:59:46 +0200

    La lista de tareas relaccionadas ha sido añadida al archivo app.py

--- app.py
+++ app.py
@@ -1,1 +1,3 @@
 # Este archivo es el archivo Python principal del repositorio, en el la estructura básica en Flask tiene que ser creada y manejada.
+# Tareas relaccionadas:
+#   #1 - Crear la estructura básica de un proyecto en Flask
